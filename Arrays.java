5b6703b2819c0055a423efe9f60cad110940857a
DarioSierra00 <dario.sierramartinez@gmail.com>
2023-03-03 14:22:27 +0100

    Jarra

--- Arrays.java
+++ Arrays.java
@@ -2,17 +2,10 @@ package com.edu;
 
 import java.util.Scanner;
 
-public class Arrays {
+//public class Arrays {
 
-	public static void main(String[] args) {
-		
-		/*Realizar un programa que solicite diez números enteros y cuando se
-		termine de introducir el último los muestre en el orden inverso al que se
-		introdujeron.*/
-		Scanner sc = new Scanner(System.in);
-		
-		int numeros[] = new int[10];
-		
+	//public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
 		/* for(int i = 0; i <numeros.length; i++) {
 			 System.out.println("Ingrese los numeros que quiera: ");
 			 numeros[i] = Integer.valueOf(sc.nextLine());
@@ -20,32 +13,56 @@ public class Arrays {
 		 }
 		 for(int j = 0; j < numeros.length; j++) {
 			 System.out.println(numeros.length-numeros[j]+1);
-		 }
-		 /*
-		  Realiza un programa que cargue un vector con de 10 números enteros.
-El programa debe imprimir los elementos del vector y después
-desplazarlos una posición, de tal forma que el último pase a la primera
-posición, el primero a la segunda, el segundo a la tercera, y así
-sucesivamente.
-Introduce el número en la posición (1/10): 5
-Introduce el número en la posición (2/10): 10
-...
-Introduce el número en la posición (10/10): 9
-Vector original: 5 10 2 30 4 15 6 88 3 9
-Vector después de desplazar: 9 5 10 2 30 4 15 6 88 3
-NOTA: Sólo debe usarse un vector, no crear vectores auxiliares.
-		  */
-		int tmp =numeros[0];
-		for(int i = 0; i <numeros.length; i++) {
-			 System.out.println("Introduce el numero en la posicion: ");
-			 numeros[i] = Integer.valueOf(sc.nextLine());
-		 }
-		 for(int j = 0; j< numeros.length; j++) {
-			 numeros[j]=tmp;
-			 System.out.println(numeros[j]);
-		 }
-		 
+		 }*/
+		
+		/*int[] numeros = {1,3,5,7,9};
+		
+		public static void desplazarDerecha(int[] numbers) {
+			int ultimo = numbers[numbers.length-1];
+			
+			for (int i = numbers.length-2;i>=0;i--) {
+				numbers[i+1]=numbers[i];
+			}
+			numbers[0]=ultimo;
+		}
+		public static void desplazarIzquierda(int[] numbers) {
+			int primero = numbers[numbers.length-1];
+			
+			for (int i = numbers.length-2;i>=0;i--) {
+				numbers[i+1]=numbers[i];
+			}
+			numbers[numbers.length-1]=primero;
+		}
+	
 	}
+}*/
+		/*Realizar un programa que solicite por teclado números entre 1 y 1000
+		hasta que el usuario informe de que ya no desea introducir más
+		números. Después debe mostrar:
+		•
+		•
+		•
+		•
+		La media de los números leídos.
+		Cuántos números terminan en cada uno de los dígitos (0 .. 9).
+		El dígito en el que más números terminan.
+		En qué dígitos no ha terminado ningún número.
+		Pista: crear un vector de contadores de 10 elementos.
+		*/
+		
+
+		
+		/*do {
+			System.out.println("Introduce numeros y 0 para terminar");
+			numeros= Integer.valueOf(sc.nextLine());
+			
+		}while(num!=0 || num>0 && num<1000);
+			*/
+	
+		
+		
+		
+	
 	
 
-}
+
---
+++ jarra/model/Jarra.java
@@ -0,0 +1,82 @@
+package com.edu.jarra.model;
+
+import java.util.Objects;
+
+public class Jarra {
+	
+	private double totalAgua;
+	private double capacidad;
+	private double cantidadAgua;
+	
+	public Jarra() {
+		super();
+	}
+
+
+	public Jarra(double totalAgua, double capacidad, double cantidadAgua) {
+		super();
+		this.totalAgua = totalAgua;
+		this.capacidad = capacidad;
+		this.cantidadAgua = cantidadAgua;
+	}
+
+	public double getTotalAgua() {
+		return totalAgua;
+	}
+
+
+	public void setTotalAgua(double totalAgua) {
+		this.totalAgua = totalAgua;
+	}
+
+
+	public double getCapacidad() {
+		return capacidad;
+	}
+
+
+	public void setCapacidad(double capacidad) {
+		this.capacidad = capacidad;
+	}
+
+
+	public double getCantidadAgua() {
+		return cantidadAgua;
+	}
+
+
+	public void setCantidadAgua(double cantidadAgua) {
+		this.cantidadAgua = cantidadAgua;
+	}
+
+	public void llenarJarra() {
+		this.cantidadAgua = this.capacidad;
+	}
+	
+	public void vaciarJarra() {
+		this.cantidadAgua = 0;
+	}
+	public boolean volcarJarra(Jarra jarra) {
+		boolean resultado = false;
+		if(jarra.capacidad>= this.cantidadAgua + jarra.cantidadAgua) {
+			jarra.cantidadAgua+=this.cantidadAgua;
+			this.cantidadAgua = 0.0;
+			resultado = true;
+		}return resultado;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Jarra other = (Jarra) obj;
+		return Double.doubleToLongBits(cantidadAgua) == Double.doubleToLongBits(other.cantidadAgua)
+				&& Double.doubleToLongBits(capacidad) == Double.doubleToLongBits(other.capacidad);
+	}
+	
+	
+}
